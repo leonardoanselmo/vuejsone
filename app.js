a5983e3adc269e7c6ae8e8998aa91b1d02dc31e3
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-12-03 20:04:09 -0300

    Arquivo app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ Vue.filter('situacaoLabel', function(situacao){
         return "Paga";
     }
 });
-
 Vue.filter('statusGeneral', function(value) {
     if(value === false){
         return "Nenhuma conta cadastrada";
@@ -16,42 +15,106 @@ Vue.filter('statusGeneral', function(value) {
     } else {
         return "Existem "+value+" contas a serem pagas.";
     }
-})
-var app = new Vue({
-    el: "#app",
-    data: {
-        test: '',
-        title: "Contas a pagar",
-        menu: [
-            {id: 0, name: "Listar contas"},
-            {id: 1, name: "Criar conta"}
-        ],
-        formType: 'insert',
-        activedView: 0,
-        camposConta: {
-            data_vcto: '',
-            descricao: '',
-            valor: '',
-            situacao: false
-        },
-        descricaoContas: [
-            'Conta de luz',
-            'Conta de água',
-            'Conta de telefone',
-            'Supermercado',
-            'Cartão de Crédito',
-            'Empréstimo',
-            'Gasolina'
-        ],
-        contas: [
-            { data_vcto: '20/08/2016', descricao: 'Conta de luz', valor: 151.23, situacao: true },
-            { data_vcto: '21/08/2016', descricao: 'Conta de água', valor: 82.10, situacao: false },
-            { data_vcto: '22/08/2016', descricao: 'Conta de telefone', valor: 45.10, situacao: false },
-            { data_vcto: '23/08/2016', descricao: 'Supermercado', valor: 102.89, situacao: false },
-            { data_vcto: '24/08/2016', descricao: 'Cartão de Crédito', valor: 1500.99, situacao: false },
-            { data_vcto: '25/08/2016', descricao: 'Empréstimo', valor: 300.00, situacao: false },
-            { data_vcto: '26/08/2016', descricao: 'Gasolina', valor: 55.30, situacao: false }
-        ]
+});
+var appComponent = Vue.extend({
+    template: `
+    <h1>{{ title }}</h1>
+    <h3 :class="{'gray': status === false, 'green': status === 0, 'red': status > 0}">
+        {{ status | statusGeneral }}</h3>
+    <nav>
+        <ul>
+            <li v-for="item in menu">
+                <a href="#" @click.prevent="showView(item.id)">{{ item.name }}</a>
+            </li>
+        </ul>
+    </nav>
+    <div v-if="activedView == 0">
+        <table border="1" cellpadding="10">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Vencimento</th>
+                <th>Descrição</th>
+                <th>Valor</th>
+                <th>Situação</th>
+                <th>Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(index, conta) in contas">
+                <td>{{ index + 1 }}</td>
+                <td>{{ conta.data_vcto }}</td>
+                <td>{{ conta.descricao }}</td>
+                <td>{{ conta.valor | currency 'R$ ' 2 }}</td>
+                <td class="minha-classe" :class="{'pago': conta.situacao, 'nao-pago': !conta.situacao}">
+                    {{ conta.situacao | situacaoLabel }}
+                </td>
+                <td>
+                    <a href="#" @click.prevent="editarConta(conta)">Editar</a> |
+                    <a href="#" @click.prevent="excluirConta(conta)">Excluir</a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div v-if="activedView == 1">
+        <form name="form" @submit.prevent="submit">
+            <label>Vencimento: </label>
+            <input type="text" v-model="camposConta.data_vcto"/>
+            <br/><br/>
+            <label>Nome: </label>
+            <select v-model="camposConta.descricao">
+                <option v-for="conta in descricaoContas" value="{{ conta }}">
+                    {{ conta }}
+                </option>
+            </select>
+            <br/><br/>
+            <label>Valor: </label>
+            <input type="text" v-model="camposConta.valor"/>
+            <br/><br/>
+            <label>Pago?: </label>
+            <input type="checkbox" v-model="camposConta.situacao"/>
+            <br/><br/>
+
+            <input type="submit" value="Enviar"/>
+        </form>
+    </div>
+    `,
+    data: function(){
+        return {
+            test: '',
+            title: "Contas a pagar",
+            menu: [
+                {id: 0, name: "Listar contas"},
+                {id: 1, name: "Criar conta"}
+            ],
+            formType: 'insert',
+            activedView: 0,
+            camposConta: {
+                data_vcto: '',
+                descricao: '',
+                valor: '',
+                situacao: false
+            },
+            descricaoContas: [
+                'Conta de luz',
+                'Conta de água',
+                'Conta de telefone',
+                'Supermercado',
+                'Cartão de Crédito',
+                'Empréstimo',
+                'Gasolina'
+            ],
+            contas: [
+                {data_vcto: '20/08/2016', descricao: 'Conta de luz', valor: 151.23, situacao: true},
+                {data_vcto: '21/08/2016', descricao: 'Conta de água', valor: 82.10, situacao: false},
+                {data_vcto: '22/08/2016', descricao: 'Conta de telefone', valor: 45.10, situacao: false},
+                {data_vcto: '23/08/2016', descricao: 'Supermercado', valor: 102.89, situacao: false},
+                {data_vcto: '24/08/2016', descricao: 'Cartão de Crédito', valor: 1500.99, situacao: false},
+                {data_vcto: '25/08/2016', descricao: 'Empréstimo', valor: 300.00, situacao: false},
+                {data_vcto: '26/08/2016', descricao: 'Gasolina', valor: 55.30, situacao: false}
+            ]
+        };
     },
     computed: {
         status: function () {
@@ -101,5 +164,9 @@ var app = new Vue({
 
         }
     }
+});
+Vue.component('app-component', appComponent);
+var app = new Vue({
+    el: "#app",
 });
 
