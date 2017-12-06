48816d29d66c177adf3623100d773942f54a29a6
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-12-06 11:29:00 -0300

    Arquivo main.js

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 var router = new VueRouter();
+var mainComponent = Vue.extend({
+    components: {
+        'bill-pay-component': billPayComponent
+    },
+    template: '<bill-pay.component></bill-pay.component>',
+    data: function(){
+        return {
+            contas: [
+                {data_vcto: '20/08/2016', descricao: 'Conta de luz', valor: 151.23, situacao: true},
+                {data_vcto: '21/08/2016', descricao: 'Conta de água', valor: 82.10, situacao: false},
+                {data_vcto: '22/08/2016', descricao: 'Conta de telefone', valor: 45.10, situacao: false},
+                {data_vcto: '23/08/2016', descricao: 'Supermercado', valor: 102.89, situacao: false},
+                {data_vcto: '24/08/2016', descricao: 'Cartão de Crédito', valor: 1500.99, situacao: false},
+                {data_vcto: '25/08/2016', descricao: 'Empréstimo', valor: 300.00, situacao: false},
+                {data_vcto: '26/08/2016', descricao: 'Gasolina', valor: 55.30, situacao: false}
+            ]
+        };
+    }
+})
 
 router.map({
     '/bills': {
@@ -9,6 +28,10 @@ router.map({
         name: 'bill.create',
         component: criarcontasComponent
     },
+    'bill/:index/update': {
+        name: 'bill.update',
+        component: criarcontasComponent
+    },
     '*': {
         component: listacontasComponent
     }
@@ -16,7 +39,7 @@ router.map({
 
 router.start({
     components: {
-        'app-component': appComponent
+        'main-component': mainComponent
     }
 }, '#app');
 
