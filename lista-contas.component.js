7b5986c4f336235d5e94d9627b84dedf6eb7c6aa
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-12-06 11:28:40 -0300

    Arquivo lista-contas.component.js

--- lista-contas.component.js
+++ lista-contas.component.js
@@ -1,4 +1,4 @@
-window.listacontasComponent = Vue.extend({
+window.listaPaycontasComponent = Vue.extend({
     template: `
     <style type="text/css">
         .pago {
@@ -29,7 +29,7 @@ window.listacontasComponent = Vue.extend({
                 {{ conta.situacao | situacaoLabel }}
             </td>
             <td>
-                <a href="#" @click.prevent="editarConta(conta)">Editar</a> |
+                <a v-link="{ name: 'bill.update', params: {index: index} }">Editar</a> |
                 <a href="#" @click.prevent="excluirConta(conta)">Excluir</a>
             </td>
         </tr>
@@ -38,33 +38,15 @@ window.listacontasComponent = Vue.extend({
     `,
     data: function() {
         return {
-            contas: [
-                {data_vcto: '20/08/2016', descricao: 'Conta de luz', valor: 151.23, situacao: true},
-                {data_vcto: '21/08/2016', descricao: 'Conta de água', valor: 82.10, situacao: false},
-                {data_vcto: '22/08/2016', descricao: 'Conta de telefone', valor: 45.10, situacao: false},
-                {data_vcto: '23/08/2016', descricao: 'Supermercado', valor: 102.89, situacao: false},
-                {data_vcto: '24/08/2016', descricao: 'Cartão de Crédito', valor: 1500.99, situacao: false},
-                {data_vcto: '25/08/2016', descricao: 'Empréstimo', valor: 300.00, situacao: false},
-                {data_vcto: '26/08/2016', descricao: 'Gasolina', valor: 55.30, situacao: false}
-            ]
+            contas: this.$root.$children[0].contas
         };
     },
     methods: {
-        editarConta: function(camposConta) {
-            this.$dispatch('change-bill', camposConta);
-            this.$dispatch('change-activedview', 1);
-            this.$dispatch('change-formtype', 'update');
-        },
         excluirConta: function(camposConta){
             if(confirm('Deseja excluir esta conta? ')){
-                this.contas.$remove(camposConta);
+                this.$root.$children[0].contas.$remove(camposConta);
             }
 
-        }
-    },
-    events: {
-        'new-bill': function(bill){
-            this.contas.push(bill);
         }
     }
 });
