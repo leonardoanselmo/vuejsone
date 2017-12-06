ee1b3e42cd50ff13dda75aa8d39d9ca50e35ae06
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-12-05 21:43:40 -0300

    Arquivo filters.js

---
+++ filters.js
@@ -0,0 +1,18 @@
+Vue.filter('situacaoLabel', function(situacao){
+    if(situacao == 0){
+        return "Não paga";
+    } else {
+        return "Paga";
+    }
+});
+Vue.filter('statusGeneral', function(value) {
+    if(value === false){
+        return "Nenhuma conta cadastrada";
+    }
+
+    if(!value){
+        return "Nenhuma conta a pagar";
+    } else {
+        return "Existem "+value+" contas a serem pagas.";
+    }
+});
