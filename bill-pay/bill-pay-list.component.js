12780c2ef351724d7f4d5735992da29fba67c36d
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-12-15 21:45:37 -0300

    Arquivo bill-pay-list.component.js

--- bill-pay/bill-pay-list.component.js
+++ bill-pay/bill-pay-list.component.js
@@ -10,38 +10,41 @@ window.billPayListComponent = Vue.extend({
     </style>
     <table border="1" cellpadding="10">
         <thead>
-        <tr>
-            <th>#</th>
-            <th>Vencimento</th>
-            <th>Descrição</th>
-            <th>Valor</th>
-            <th>Situação</th>
-            <th>Ações</th>
-        </tr>
+            <tr>
+                <th>#</th>                
+                <th>Código</th>
+                <th>Vencimento</th>
+                <th>Descrição</th>
+                <th>Valor</th>
+                <th>Situação</th>
+                <th>Ações</th>                
+            </tr>
         </thead>
         <tbody>
-        <tr v-for="(index,o) in bills">                
-            <td>{{ index + 1 }}</td>
-            <td>{{ o.date_due }}</td>
-            <td>{{ o.name }}</td>
-            <td>{{ o.value | currency 'R$ ' 2 }}</td>
-            <td class="minha-classe" :class="{'pago': o.done, 'nao-pago': !o.done}">
-                {{ o.done | situacaoLabel }}
-            </td>
-            <td>
-                <a v-link="{ name: 'bill-pay.update', params: {id: o.id} }">Editar</a> |
-                <a href="#" @click.prevent="excluirConta(o)">Excluir</a>
-            </td>
-        </tr>
+            
+            <tr v-for="(index,o) in bills">                             
+                <td>{{ index + 1 }}</td>
+                <td>{{ o.id }}</td>
+                <td>{{ o.date_due }}</td>
+                <td>{{ o.name }}</td>
+                <td>{{ o.value | currency 'R$ ' 2 }}</td>
+                <td class="minha-classe" :class="{'pago': o.done, 'nao-pago': !o.done}">
+                    {{ o.done | situacaoLabel }}
+                </td>
+                <td>
+                    <a v-link="{ name: 'bill-pay.update', params: {id: o.id} }">Editar</a> |
+                    <a href="#" @click.prevent="excluirConta(o)">Excluir</a>
+                </td>
+            </tr>
+                        
         </tbody>
-     </table>        
+     </table>            
     `,
     http: {
         root: 'http://192.168.10.10:8000/api'
     },
     data: function() {
         return {
-            // contas: this.$root.$children[0].billsPay
             bills: []
         };
     },
