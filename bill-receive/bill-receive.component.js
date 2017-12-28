76f47e6c423409ec5685ea837d23170c34e2d36c
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-12-27 22:06:18 -0300

    Arquivo biil-receive.component.js

--- bill-receive/bill-receive.component.js
+++ bill-receive/bill-receive.component.js
@@ -18,11 +18,11 @@ window.billReceiveComponent = Vue.extend({
         }
     </style>
     <h1>{{ title }}</h1>
-    <!--<h3 :class="{'gray': status === false, 'green': status === 0, 'red': status > 0}">
+    <h3 :class="{'gray': status === false, 'green': status === 0, 'red': status > 0}">
         {{ status | statusGeneral }}
     </h3>
     <menu-component></menu-component>
-    <router-view></router-view>-->   
+    <router-view></router-view>   
     
     `,
     data: function(){
